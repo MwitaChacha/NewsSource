3d866d318af553053eeaf7de9775a723ebbd98b9
Mwita Chacha <jonahhchacha@gmail.com>
2021-10-31 13:20:14 +0300

    Creating a function that processes the results

--- app/request.py
+++ app/request.py
@@ -1,6 +1,6 @@
 from app import app
 import urllib.request,json
-from .models import source,article
+from .models import Source,Article
 
 # Getting api key
 apiKey = app.config['API_KEY']
@@ -23,5 +23,29 @@ def get_sources():
         if get_sources_response["sources"]:
             source_results_list = get_sources_response['sources']
             source_results = process_results(source_results_list)
+
+    return source_results
+
+def process_results(source_list):
+    """
+    Function that processes the source result and transform them into a list of objects
+    Args:
+    source_list: A list of dictionaries that contain movie details
+    Returns:
+    source_results: A list of Source objects
+    """
+    source_results=[]
+    for source_item in source_list:
+        id = source_item.get("id")
+        name = source_item.get("name")
+        description = source_item.get("description")
+        url = source_item.get("url")
+        category = source_item.get("category")
+
+
+        source_object=Source(id,name,description,url,category)
+        source_results.append(source_object)
+
+       
 
     return source_results
