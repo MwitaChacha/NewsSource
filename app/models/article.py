b19d1a5a60bc0899ede6f707ac06ff9bacf0856a
Mwita Chacha <jonahhchacha@gmail.com>
2021-10-31 13:13:35 +0300

    Creating and testing article model

---
+++ app/models/article.py
@@ -0,0 +1,12 @@
+class Article:
+    """
+    A class that generates new instances of news articles within news sources
+    """
+    
+    def __init__(self,author, title, description, url, urlToImage, publishedAt):
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
--- app/test.py
+++ app/test.py
@@ -1,10 +1,11 @@
 import unittest
-from models import source
+from models import source, article
 
 Source=source.Source
+Article=article.Article
 class SourceTest(unittest.TestCase):
     '''
-    Test Class to test the behaviour of the Movie class
+    Test Class to test the behaviour of the Source class
     '''
 
     def setUp(self):
@@ -15,3 +16,21 @@ class SourceTest(unittest.TestCase):
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_source,Source))
+        
+        
+class ArticleTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the Article class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_article = Article("John",'Python Must Be Crazy','A thrilling new Python Series','https://image.tmdb.org/t/p/w500/khsjha27hbs', 'https://image.tmdb.org/', 'October')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_article,Article))        
+        
+if __name__ == '__main__':
+    unittest.main()        
